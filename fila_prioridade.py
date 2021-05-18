f43b710a87f3bab83adb19f2c3113c69e8be8827
dojiDoMal <andersonamacedo@hotmail.com>
2021-05-18 12:12:37 -0300

    creating methodtemplate

--- fila_prioridade.py
+++ fila_prioridade.py
@@ -3,12 +3,7 @@ from fila_base import FilaBase
 
 class FilaPrioridade(FilaBase):
     def gera_senha_atual(self) -> None:
-        self.senha_atual = f'NM{self.codigo}'
-
-    def atualiza_fila(self) -> None:
-        self.reseta_fila()
-        self.gera_senha_atual()
-        self.fila.append(self.senha_atual)
+        self.senha_atual = f'PR{self.codigo}'
 
     def chama_cliente(self, caixa: int) -> str:
         cliente_atual: str = self.fila.pop(0)
